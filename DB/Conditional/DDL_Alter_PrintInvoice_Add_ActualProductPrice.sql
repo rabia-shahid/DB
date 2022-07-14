ee88f0f389f58872834c70bf336d6a2d10a3d3d1
rabia-shahid <53579930+rabia-shahid@users.noreply.github.com>
2022-07-14 21:24:48 +0500

    add FBR_Invoice column in print invoice

--- DB/Conditional/DDL_Alter_PrintInvoice_Add_ActualProductPrice.sql
+++ DB/Conditional/DDL_Alter_PrintInvoice_Add_ActualProductPrice.sql
@@ -4,3 +4,12 @@ Alter table printinvoice
 add actualProductPrice numeric(8,2)
 End
 
+GO
+
+if not Exists (Select 1 from sys.columns  where object_id=Object_Id('printinvoice') and name='FBR_Invoice')
+Begin
+Alter table printinvoice
+add FBR_Invoice varchar(50)
+End
+
+

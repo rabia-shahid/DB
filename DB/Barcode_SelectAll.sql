a1446b266bede214978da672e0ef588f2e101e04
Faris <farissimran96@gmail.com>
2022-08-26 02:07:42 +0500

    Get Barcode SP

---
+++ DB/Barcode_SelectAll.sql
@@ -0,0 +1,44 @@
+IF EXISTS (SELECT * FROM sys.objects WHERE type = 'P' AND OBJECT_ID = OBJECT_ID('dbo.Barcode_SelectAll'))
+drop procedure Barcode_SelectAll
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- ==========================================================================================
+-- Entity Name:	Barcode_SelectAll
+-- Description:	This stored procedure is intended to get all products with barcodes
+-- ==========================================================================================
+
+CREATE procedure [dbo].[Barcode_SelectAll]
+As
+Begin
+		delete from barcodes
+		insert into barcodes(barcode,itemname,articleno,qty) Select code,name,rsltr,qty	From OilProducts;
+		WITH TempEmp (barcode,duplicateRecCount)
+			AS
+			(
+			SELECT barcode,ROW_NUMBER() OVER(PARTITION by barcode ORDER BY barcode) 
+			AS duplicateRecCount
+			FROM dbo.barcodes
+			)
+			--Now Delete Duplicate Rows
+			DELETE FROM TempEmp
+			WHERE duplicateRecCount > 1 
+			 --if ( @Column = 'All' and @value = 'All') 
+				--begin
+
+					select ROW_NUMBER() OVER (ORDER BY barcode) as number, * from barcodes where LEN(barcode) <=6 
+			--	end
+			--else if ( @Column = 'Product Name' ) 
+			--	begin
+			--		select * from barcodes where LEN(barcode) <=6 and ItemName like '%' + @value + '%'
+			--	end
+			--else if ( @Column = 'Barcode' ) 
+			--	begin
+			--		select * from barcodes where LEN(barcode) <=6 and barcode like '%' + @value + '%'
+			--	end
+		 
+		
+End
